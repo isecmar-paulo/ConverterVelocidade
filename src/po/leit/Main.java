75dad015924be806a70572e6e357272d8b05fed0
Menelik <avlisontas@gmail.com>
2021-04-05 13:14:54 -0100

    Initial commit

---
+++ src/po/leit/Main.java
@@ -0,0 +1,31 @@
+package po.leit;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printConversion(19.5);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if(kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if(kilometersPerHour <0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour +
+                    " km/h= " + milesPerHour +
+                    " mi/h");
+
+        }
+    }
+}
